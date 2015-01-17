e68f58ca6cebaf6b7486f3116b7081a1b3f78ab5
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-17 18:26:22 +0000

    Created a simple add7 test program

--- src/sml/Instruction.java
+++ src/sml/Instruction.java
@@ -17,21 +17,17 @@ public abstract class Instruction {
 
 	// Constructor: an instruction with label l and opcode op
 	// (op must be an operation of the language)
-
 	public Instruction(String l, String op) {
 		this.label = l;
 		this.opcode = op;
-
 	}
 
 	// = the representation "label: opcode" of this Instruction
-
 	@Override
 	public String toString() {
 		return label + ": " + opcode;
 	}
 
 	// Execute this instruction on machine m.
-
 	public abstract void execute(Machine m);
 }
