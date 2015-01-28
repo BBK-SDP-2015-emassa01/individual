63eab69ad44a56ca430178a220172ced3cac307e
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-28 14:09:38 +0000

    test Labels class

--- src/smlTests/LabelsTest.java
+++ src/smlTests/LabelsTest.java
@@ -2,13 +2,42 @@ package smlTests;
 
 import static org.junit.Assert.*;
 
+import sml.Labels;
+import smlExceptions.*;
 import org.junit.Test;
 
 public class LabelsTest {
+	
+	private Labels test = new Labels();
+	
+	
+	@Test (expected = DuplicatesException.class)
+	public void addLabelTest() {
+		
+		test.addLabel("1");
+		test.addLabel("2");
+		test.addLabel("3");
+		test.addLabel("4");
+		
+		test.addLabel("1"); // throws DuplicatesException !!! YAY!
 
+		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addLabelTest2() {
+		
+		test.addLabel("1");
+		test.addLabel("2");
+		test.addLabel("3");
+		test.addLabel("4");
+
+		test.addLabel("5"); //should work
+		int observed = test.indexOf("5");
+		int expected = 4;
+		
+		assertEquals(expected, observed);
+		
 	}
 
 }
