bad1fa4abc5c6f9d672c7e3306330d9f80c8b0fc
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-28 14:28:02 +0000

    added description to class javadoc

--- src/smlExceptions/DuplicatesException.java
+++ src/smlExceptions/DuplicatesException.java
@@ -1,8 +1,12 @@
 package smlExceptions;
 
-
 public class DuplicatesException extends RuntimeException {
 	
+	/**
+	 * A Class to throw duplicates if there are duplicates (for example, duplicate labels within the sml program).
+	 */
+	private static final long serialVersionUID = 1L; //for serialisation.
+
 	public DuplicatesException(){
 	}
 
