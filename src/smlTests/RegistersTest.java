ec45f8690e3bd3bbf8a435990e042015a2da6286
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-28 15:49:44 +0000

    RegistersTest class

--- src/smlTests/RegistersTest.java
+++ src/smlTests/RegistersTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import sml.Registers;
+
 public class RegistersTest {
 
+	private Registers testRegisters = new Registers();
+	private int[] registers = {1,2,3,4,5,6,7,8,9};
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setRegistersTest(){
+		for (int i = 0; i != registers.length; i++) {
+			registers[i] = 0;
+		}
+		
+		testRegisters.setRegister(4, 50);
+
+		int observed = testRegisters.getRegister(4);
+		int expected = 50;
+		
+		assertEquals(expected, observed);
 	}
 
 }
