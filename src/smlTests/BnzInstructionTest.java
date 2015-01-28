a47aa3702069cdd67b9ff00c62eeabf74b81ba3b
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-28 13:36:51 +0000

    completed BnZ

--- src/smlTests/BnzInstructionTest.java
+++ src/smlTests/BnzInstructionTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import sml.AddInstruction;
 import sml.BnzInstruction;
 import sml.Machine;
 import sml.Registers;
+
 
 public class BnzInstructionTest {
 
@@ -17,16 +17,13 @@ public class BnzInstructionTest {
 	private Machine m2 = new Machine();
 	Registers r2 = new Registers();
 	Registers r = new Registers();
-	
+	BnzInstruction bnzInstructionTest2 = new BnzInstruction("bnz", result, register);
 	
 	@Test
 	public void executeTest() {
 		
 		m.setRegisters(r);
 		m2.setRegisters(r2);
-		
-		BnzInstruction bnzInstructionTest2 = new BnzInstruction("bnz", result, register);
-	
 		
 		m.getRegisters().setRegister(3,30); // check getRegisters() and setRegister() methods.
 		m.getLabels().addLabel("f1");
@@ -40,5 +37,12 @@ public class BnzInstructionTest {
 		
 		//void method so testing each component separately with println statements.
 		assertEquals(2, m.getLabels().indexOf(register)); 
+	}
+	
+	@Test
+	public void toStringTest(){
+		
+		System.out.println(bnzInstructionTest2.toString()); //this is what get's printed to String. Checking if it matches the below
+		System.out.println("bnz: bnz, register: 9, next label: f0");
 	}
 }
