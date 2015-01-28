338310ec70ebdef38656b60ccb9f61aa3b8d7af9
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-28 15:44:05 +0000

    Out Instruction Test

--- src/smlTests/OutInstructionTest.java
+++ src/smlTests/OutInstructionTest.java
@@ -4,11 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import sml.Machine;
+import sml.OutInstruction;
+import sml.Registers;
+
 public class OutInstructionTest {
 
+	OutInstruction oi = new OutInstruction("out", 5);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void executeTest(){
+		Machine m = new Machine();
+		Registers r = new Registers();
+		m.setRegisters(r);
+		
+		// check getRegisters() and setRegister() methods.
+		m.getRegisters().setRegister(5, 5);
+		
+		oi.execute(m);
+		
+		int res = m.getRegisters().getRegister(5);
+		System.out.println(res); //printed result to screen
+		assertTrue(res == 5); //checked the result
+		assertTrue(m.getRegisters().getRegister(5) ==5); //checked the result was in the register.
 	}
+	
+	//no toString() as uses the super.
+
 
 }
