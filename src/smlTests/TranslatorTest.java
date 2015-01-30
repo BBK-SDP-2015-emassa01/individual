854fd98dd66b3dce82645ef59b3c43e9a8086396
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-30 18:03:41 +0000

    Used reflection to test scanInt()
    Private methods have been tested using reflection

--- src/smlTests/TranslatorTest.java
+++ src/smlTests/TranslatorTest.java
@@ -2,7 +2,6 @@ package smlTests;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -15,10 +14,14 @@ import sml.*;
 public class TranslatorTest {
 
 	Machine m = new Machine();
+	Machine m2 = new Machine();
 	Registers r = new Registers();
+	Registers r2 = new Registers();
 	ArrayList<Instruction> prog = new ArrayList<>();
+	ArrayList<Instruction> prog2 = new ArrayList<>();
 	
 	Labels lab = new Labels();
+	Labels lab2 = new Labels();
 	
 	@Before
 	public void initialise(){
@@ -26,6 +29,11 @@ public class TranslatorTest {
 		m.getRegisters().setRegister(3, 3);// check getRegisters() and setRegister() methods.
 		m.getRegisters().setRegister(4, 4);
 		m.getRegisters().setRegister(5, 5);
+		
+		m2.setRegisters(r2);
+		m2.getRegisters().setRegister(3, 3);// check getRegisters() and setRegister() methods.
+		m2.getRegisters().setRegister(4, 4);
+		m2.getRegisters().setRegister(5, 5);
 		
 		AddInstruction ai = new AddInstruction("add", 5, 3, 4); //create some instructions
 		SubInstruction ai2 = new SubInstruction("sub", 6, 8, 11);
@@ -38,6 +46,7 @@ public class TranslatorTest {
 		prog.add(ai4);	
 		
 		m.setProg(prog);
+		m2.setProg(prog2);
 	}
 	
 	@Test 
@@ -59,20 +68,36 @@ public class TranslatorTest {
 	@Test
 	public void scanTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Translator t = new Translator("testAdd.txt");
+		t.setLabels(lab);
+		t.setProgram(prog);
+		t.setLine("f0 lin 10 6");
+		
 		Class<?> c = t.getClass();
 		Method method = c.getDeclaredMethod("scan");
 		method.setAccessible(true);
-		method.invoke(t);
+		String obs = (String) method.invoke(t);
 		
-		String line = "f0 lin 10 6";
-		t.setLine(line);
-		System.out.println(t);
-	
+		System.out.println(t.toString());
+		String expected = "f0";
+		assertEquals(expected, obs);
+
 	}
 	
-	public void scanIntTest(){
+	@Test
+	public void scanIntTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Translator t2 = new Translator("testAdd.txt");
+		t2.setLabels(lab2);
+		t2.setProgram(prog2);
+		t2.setLine("10 6");
 		
+		Class<?> c = t2.getClass();
+		Method method = c.getDeclaredMethod("scanInt");
+		method.setAccessible(true);
+		int obs = (Integer) method.invoke(t2);
 		
+		System.out.println(t2.toString());
+		int expected = 10;
+		assertEquals(expected, obs);
 	}
 
 }
